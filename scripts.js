22f6c37c033d71a3d187bd827d54951cfb5bdf98
pdouser <155661734+pdouser@users.noreply.github.com>
2024-01-10 16:21:34 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,5 +2,7 @@ function dateFormat() {
   document.getElementById("barchart-request-date").value = new Date()
     .toISOString()
     .substring(0, 10);
+    document.getElementById("debt-request-date").value = new Date()
+    .toISOString()
+    .substring(0, 10);
 }
-
